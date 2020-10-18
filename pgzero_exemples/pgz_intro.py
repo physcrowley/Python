c1e6ec4c8e2dba3f0707132549d79c80257d4f42
physcrowley <crowlda@ecolecatholique.ca>
2020-10-18 16:06:47 -0400

    moved some files

---
+++ pgzero_exemples/pgz_intro.py
@@ -0,0 +1,11 @@
+import pgzrun # pas nécessaire avec Thonny
+
+WIDTH = 400
+HEIGHT = 300
+
+def draw():
+    screen.fill("red")
+    screen.blit("franco-cite.jpg",(50,50)) # chemin de l'image, position dans la fenêtre
+
+
+pgzrun.go() # pas nécessaire avec Thonny
