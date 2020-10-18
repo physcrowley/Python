40a55c0beabdb4b4c0029501982a88b35ab1ad6b
physcrowley <crowlda@ecolecatholique.ca>
2020-10-18 16:13:55 -0400

    clarification for `python` versus `pgzrun` launch

--- pgzero_exemples/pgz_intro.py
+++ pgzero_exemples/pgz_intro.py
@@ -1,4 +1,4 @@
-import pgzrun # pas nécessaire avec Thonny
+import pgzrun # pas nécessaire avec Thonny en mode Pygame Zero
 
 WIDTH = 400
 HEIGHT = 300
@@ -8,4 +8,14 @@ def draw():
     screen.blit("franco-cite.jpg",(50,50)) # chemin de l'image, position dans la fenêtre
 
 
-pgzrun.go() # pas nécessaire avec Thonny
+pgzrun.go() # pas nécessaire avec Thonny en mode Pygame Zero
+
+"""
+les lignes `import pgzrun` et `pgzrun.go()` sont essentiels pour
+tout programme qui est lancé comme un programme Python typique (avec le
+bouton "Run" ou avec la commande `python` au console).
+
+Il ne faut pas les avoir dans le code si tu lances le programme avec la
+commande `pgzrun` au console ou avec un éditeur comme Thonny en mode
+Pygame Zero
+"""
---
+++ pgzero_exemples/pgz_intro_modePGZ.py
@@ -0,0 +1,10 @@
+# cette version est pour un lancement avec Thonny en mode Pygame Zero
+# ou avec la commande `pgzrun` au console (au lieu de `python`)
+
+WIDTH = 400
+HEIGHT = 300
+
+def draw():
+    screen.fill("red")
+    screen.blit("franco-cite.jpg",(50,50)) # chemin de l'image, position dans la fenêtre
+
