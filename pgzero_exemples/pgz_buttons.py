8909504a674d861d339c28ccb2ee70700473fa1c
David Crowley <CROWLDA@ecolecatholique.ca>
2020-10-27 12:34:12 -0400

    initialised unused buttons example

---
+++ pgzero_exemples/pgz_buttons.py
@@ -0,0 +1,17 @@
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+
+# définir un rectangle pour un textbox
+
+
+# définir une action pour quand on clique sur le textbox
+
+
+def draw():
+    screen.clear()
+
+
+
+pgzrun.go()
