43b7044c5efd4d3079de121059bdcd6f8145dda5
David Crowley <CROWLDA@ecolecatholique.ca>
2020-10-28 11:35:14 -0400

    made modular but broke the pgzero game

--- pgzero_exemples/pgz_timer_with_object.py
+++ pgzero_exemples/pgz_timer_with_object.py
@@ -16,15 +16,16 @@ class game_time:
     def set_time(self):  # called at an interval of 1.0 seconds by the clock
         self.elapsed_time += 1
 
-t = game_time() # t refers to a game_time object -> we can now access t
-                # within our other functions
-
 def draw():
     screen.clear()
     message = str(t.elapsed_time) + " seconds"
     screen.draw.text(message, midtop = (WIDTH/2, HEIGHT/2 - 5))
-    
-# calls t.set_time() function every second
-clock.schedule_interval(t.set_time, 1.0)
 
-pgzrun.go()
+if __name__ == "__main__":
+    t = game_time() # t refers to a game_time object -> we can now access t
+                # within our other functions
+    
+    # calls t.set_time() function every second
+    clock.schedule_interval(t.set_time, 1.0)
+
+    pgzrun.go()
